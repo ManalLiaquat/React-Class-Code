0ee58ac20712aa66e11da7ba02e4e75affdde2a3
ManalLiaquat <m.manalliaquat@gmail.com>
2018-08-26 14:36:01 +0500

    Creating todo list app in react

--- src/App.js
+++ src/App.js
@@ -5,8 +5,58 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      todos: [],
+      text: ""
+    };
+    this.add = this.add.bind(this);
+    this.updateText = this.updateText.bind(this);
   }
+
+  /* Functional functions */
+
+  updateText(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  add() {
+    const { text, todos } = this.state;
+    todos.push(text);
+    this.setState({ todos, text: "" });
+
+    console.log(this.state.todos);
+  }
+
+  edit() {
+    const { todos } = this.state;
+
+  }
+
+  delete(index) {
+    const { todos } = this.state;
+
+  }
+
+  renderTodos() {
+    const { todos } = this.state;
+    return (
+      <ol>
+        {todos.map((item, index) => {
+          return (
+            <li key={`${item}_${index}`}>
+              {item}
+              <button onClick={this.edit.bind(this, index)}>Edit</button>
+              <button onClick={this.delete.bind(this, index)}>Delete</button>
+            </li>
+          );
+        })}
+      </ol>
+    );
+  }
+
+  /* Body functions */
 
   renderHeader() {
     return (
@@ -18,7 +68,19 @@ class App extends Component {
   }
 
   renderBody() {
-    return <div>...body</div>;
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter something"
+          onChange={this.updateText}
+          value={this.state.text}
+        />
+        <button onClick={this.add}>Add</button>
+        <hr />
+        {this.renderTodos()}
+      </div>
+    );
   }
 
   renderFooter() {
