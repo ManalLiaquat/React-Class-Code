823953fb91d6ee610c2e93e5c28393836c597c38
ManalLiaquat <m.manalliaquat@gmail.com>
2018-08-29 01:13:46 +0500

    Adding update function

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ class App extends Component {
     super();
     this.state = {
       todos: [],
-      text: ""
+      text: "",
+      currentIndex: null
     };
     this.add = this.add.bind(this);
     this.updateText = this.updateText.bind(this);
+    this.cancel = this.cancel.bind(this);
+    this.updateTodo = this.updateTodo.bind(this);
   }
 
   /* Functional functions */
@@ -29,14 +32,25 @@ class App extends Component {
     console.log(this.state.todos);
   }
 
-  edit() {
-    const { todos } = this.state;
+  updateTodo() {
+    const { todos, text, currentIndex } = this.state;
+    todos[currentIndex] = text;
+    this.setState({ text: todos[currentIndex], currentIndex: null });
+  }
 
+  edit(index) {
+    const { todos } = this.state;
+    this.setState({ text: todos[index], currentIndex: index });
   }
 
   delete(index) {
     const { todos } = this.state;
+    todos.splice(index, 1);
+    this.setState({ todos, currentIndex: null });
+  }
 
+  cancel() {
+    this.setState({ text: "", currentIndex: null });
   }
 
   renderTodos() {
@@ -58,22 +72,35 @@ class App extends Component {
 
   /* Body functions */
 
-
   renderBody() {
+    const { currentIndex } = this.state;
+
     return (
       <div>
+        <hr/>
         <input
           type="text"
           placeholder="Enter something"
           onChange={this.updateText}
           value={this.state.text}
         />
-        <button onClick={this.add}>Add</button>
+        {currentIndex == null ? (
+          <button onClick={this.add}>Add</button>
+        ) : (
+          <span>
+            <button onClick={this.updateTodo}>Update</button>
+            <button onClick={this.cancel}>Cancel</button>
+          </span>
+        )}
         <hr />
+        {currentIndex != null && (
+          <p>You are editing item # {currentIndex + 1} currently</p>
+        )}
         {this.renderTodos()}
       </div>
     );
-  }sd
+  }
+  sd;
   renderHeader() {
     return (
       <header className="App-header">
