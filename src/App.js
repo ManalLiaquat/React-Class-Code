3bb568d52813967b8c024bd3c30a87db20d61c12
ManalLiaquat <m.manalliaquat@gmail.com>
2018-09-15 14:54:12 +0500

    Creating github issues page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Container from "./Components/Container/Container";
+// import Container from "./Screens";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      issues: [
+        {
+          title: "dummy title 1",
+          comments: 4,
+          date: new Date(),
+          isOpen: true,
+          isFavourite: false
+        },
+        {
+          title: "dummy title 2",
+          comments: 9,
+          date: new Date(),
+          isOpen: true,
+          isFavourite: false
+        }
+      ],
+      text: null,
+      toggleHeart: false
+    };
+    this.toggleHeart = this.toggleHeart.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  toggleOpen(i) {
+    const { issues } = this.state;
+    issues[i].isOpen = !issues[i].isOpen;
+    this.setState({ issues: issues });
+  }
+
+  toggleHeart(i) {
+    const { issues } = this.state;
+    issues[i].isFavourite = !issues[i].isFavourite;
+    this.setState({ issues: issues });
   }
 
   /* Body functions */
@@ -21,13 +54,51 @@ class App extends Component {
   }
 
   renderBody() {
-    const { quizzes, infoBody, quizInfo } = this.state;
-    return <div>
-        <h1>(App.js) not a children Container</h1>
-        <Container>
-          <h1>(App.js) children Container</h1>
-        </Container>
-      </div>;
+    const { issues } = this.state;
+    return (
+      <div>
+        <center>
+          <input
+            type="text"
+            style={{ width: "80%" }}
+            onChange={e => this.setState({ text: e.target.value })}
+          />
+        </center>
+        <ul style={{ listStyle: "none" }}>
+          {issues.map((v, i) => {
+            return (
+              <li key={i}>
+                <h3>{v.title}</h3>
+                <p>Comments: {v.comments}</p>
+                <pre>
+                  Created on:
+                  {v.date.toDateString()} {v.date.toLocaleTimeString()}
+                </pre>
+                {/*&nbsp;*/}
+                <p>
+                  <i
+                    onClick={() => this.toggleHeart(i)}
+                    style={{ color: "red" }}
+                    className={
+                      issues[i].isFavourite ? "fa fa-heart" : "fa fa-heart-o"
+                    }
+                  />
+                </p>
+                <p>
+                  <i
+                    onClick={() => this.toggleOpen(i)}
+                    style={{ color: "green" }}
+                    className={
+                      issues[i].isOpen ? "fa fa-check-circle" : "fa fa-ban"
+                    }
+                  />
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 
   renderFooter() {
@@ -45,10 +116,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        {this.renderHeader()}
-        {this.renderBody()}
-        {this.renderFooter()}
+      <div>
+        <div className="App">{this.renderHeader()}</div>
+        <div>{this.renderBody()}</div>
+        <div className="App">{this.renderFooter()}</div>
       </div>
     );
   }
