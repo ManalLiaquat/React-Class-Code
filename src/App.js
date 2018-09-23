8abea43e1a879f1dffc1b5e24c659369aa7ded3f
Manal Liaquat <m.manalliaquat@gmail.com>
2018-09-23 12:11:18 +0500

    learning new life cycle hooks

--- src/App.js
+++ src/App.js
@@ -1,156 +1,80 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Container from "./Screens/Container/Container";
+// import Container from "./Screens/Container/Container";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      issues: [
-        {
-          title: "dummy title 1",
-          comments: 4,
-          date: new Date(),
-          isOpen: false,
-          isFavourite: false
-        },
-        {
-          title: "dummy title 2",
-          comments: 9,
-          date: new Date(),
-          isOpen: true,
-          isFavourite: false
-        }
-      ],
-      filteredIssues: [],
-      text: null,
-      toggleHeart: false,
-      searchIssue: null,
-      text: "Hello World"
-    };
-    this.toggleHeart = this.toggleHeart.bind(this);
-    this.toggleOpen = this.toggleOpen.bind(this);
-    this.searchIssue = this.searchIssue.bind(this);
-
-    // this.newFilter = this.newFilter.bind(this);
+    this.state = { text: "Saylani" };
   }
 
-  searchIssue(e) {
-    var text = e.target.value;
-    // const { text } = this.state;
-    // console.log(text);
-    // var i = 1;
-    // console.log(i++, "******");
-    const { issues, filteredIssues } = this.state;
-    issues
-      .filter(item => {
-        return item.isOpen
-          ? text === "open"
-            ? item.isOpen
-            : text === "close"
-              ? !item.isOpen
-              : null
-          : text === "close"
-            ? !item.isOpen
-            : text === "open"
-              ? item.isOpen
-              : null;
-      })
-      .map(v => console.log("obj****", v));
-    // .map(v => console.log(v,"obj"));
-    // debugger;
-    // if (text === "open") {
-    //   debugger;
-    //   this.setState({ searchIssue: true });
-    // } else if (text === "close") {
-    //   debugger;
-    //   this.setState({ searchIssue: false });
-    // }
-  }
-  // newFilter() {
-  //   const { searchIssue, issues } = this.state;
-  //   issues
-  //     .filter(item => {
-  //       return item.isOpen ? searchIssue : !searchIssue;
-  //     })
-  //     .map(v => console.log(v.title));
-  // }
-
-  toggleOpen(i) {
-    const { issues } = this.state;
-    issues[i].isOpen = !issues[i].isOpen;
-    this.setState({ issues: issues });
+  /*  */
+  static getDerivedStateFromProps(props, state) {
+    console.log("======================");
+    console.log(props, state);
+    console.log("======================");
+    return { text: state.text };
   }
 
-  toggleHeart(i) {
-    const { issues } = this.state;
-    issues[i].isFavourite = !issues[i].isFavourite;
-    this.setState({ issues: issues });
+  componentWillReceiveProps(nextProps) {
+    console.log("componentWillReceiveProps");
+  }
+  // jab bhi state change hogi child ka event zaroor chalega
+
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+
+  componentDidMount() {
+    // recommended
+    console.log("componentDidMount");
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  componentDidCatch() {
+    console.log("componentDidCatch");
   }
 
   /* Body functions */
+
+  renderBody() {
+    const { text } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={e => {
+            this.setState({ text: e.target.value });
+          }}
+        />
+      </div>
+    );
+  }
 
   renderHeader() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">quiz app</h1>
+        <h1 className="App-title">Hello World</h1>
       </header>
-    );
-  }
-
-  renderBody() {
-    const { issues } = this.state;
-    return (
-      <div>
-        <center>
-          <input
-            type="text"
-            style={{ width: "80%" }}
-            onChange={e => {
-              // this.setState({ text: e.target.value })
-              // this.newFilter();
-              this.searchIssue(e);
-            }}
-            autoFocus={true}
-            // onChange={this.newFilter}
-          />
-        </center>
-        <ul style={{ listStyle: "none" }}>
-          {issues.map((v, i) => {
-            return (
-              <li key={i}>
-                <h3>{v.title}</h3>
-                <p>Comments: {v.comments}</p>
-                <pre>
-                  Created on:
-                  {v.date.toDateString()} {v.date.toLocaleTimeString()}
-                </pre>
-                {/*&nbsp;*/}
-                <p>
-                  <i
-                    onClick={() => this.toggleHeart(i)}
-                    style={{ color: "red" }}
-                    className={
-                      issues[i].isFavourite ? "fa fa-heart" : "fa fa-heart-o"
-                    }
-                  />
-                </p>
-                <p>
-                  <i
-                    onClick={() => this.toggleOpen(i)}
-                    style={{ color: "green" }}
-                    className={
-                      issues[i].isOpen ? "fa fa-check-circle" : "fa fa-ban"
-                    }
-                  />
-                </p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
     );
   }
 
@@ -168,18 +92,12 @@ class App extends Component {
   }
 
   render() {
-    const { text } = this.state;
+    // const { text } = this.state;
     return (
       <div className="App">
-        <div className="App">{this.renderHeader()}</div>
-        {/* <div>{this.renderBody()}</div> */}
-        <div className="App">{this.renderFooter()}</div>
-        <input
-          type="text"
-          autoFocus={true}
-          onChange={e => this.setState({ text: e.target.value })}
-        />
-        <Container text={text} />
+        {this.renderHeader()}
+        {this.renderBody()}
+        {this.renderFooter()}
       </div>
     );
   }
