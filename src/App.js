5cfbba43ce6a5930b97233dec3b0a3487066e5b3
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-13 22:17:59 +0500

    implimenting facebook login with firebase

--- src/App.js
+++ src/App.js
@@ -1,166 +1,84 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-const AbortController = window.AbortController;
-const controller = new AbortController();
-const signal = controller.signal;
-
-const PATH_BASE = "https://hn.algolia.com/api/v1";
-const PATH_SEARCH = "/search";
-const PARAM_SEARCH = "query=";
-const PARAM_PAGE = "page=";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+import firebase from "./Config/firebase";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      searchTerm: "React",
-      result: null,
-      i: 0,
-      pagination: 0
+      coords: { latitude: 24.8830931, longitude: 67.0685517 }
     };
+    this.signWithFacebook = this.signWithFacebook.bind(this);
+    this.updateCoords = this.updateCoords.bind(this);
+    this.getPosition = this.getPosition.bind(this);
   }
 
-  search(searchTerm) {
-    let { i, result } = this.state;
-    fetch(
-      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&&${PARAM_PAGE}${i}`,
-      {
-        method: "get",
-        signal: signal
-      }
-    )
-      .then(res => res.json())
-      .then(newResult => {
-        console.log("Getting data***");
-        this.setState({ result: newResult.hits });
-        if (i > 0) {
-          this.setState({
-            result: result.concat(newResult.hits)
-          });
-        }
-      })
-      .catch(e => alert(e.message));
+  getPosition() {
+    navigator.geolocation.getCurrentPosition(res => {
+      this.setState({ coords: res.coords });
+    });
+  }
+
+  updateCoords(latitude, longitude) {
+    this.setState({ coords: { latitude, longitude } });
   }
 
   componentDidMount() {
-    window.addEventListener("scroll", this.onScroll, false);
+    this.getPosition();
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.onScroll, false);
-  }
+  signWithFacebook() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        console.log(token);
 
-  onScroll = () => {
-    if (this.state.result) {
-      if (
-        window.innerHeight + window.scrollY >=
-          document.body.offsetHeight + 10 &&
-        this.state.result.length
-      ) {
-        // this.setState({ i: ++this.state.i });
-        // this.search();
-      }
-    }
-    console.log(
-      `${window.innerHeight + window.scrollY} >= ${document.body.offsetHeight +
-        10}`
-    );
-  };
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
 
-  pagination(i = 0) {
-    let { searchTerm } = this.state;
-    fetch(
-      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&&${PARAM_PAGE}${i}`,
-      {
-        method: "get",
-        signal: signal
-      }
-    )
-      .then(res => res.json())
-      .then(result => {
-        console.log("Getting data***");
-        this.setState({ result: result.hits, pagination: result.nbPages });
+        // ...
       })
-      .catch(e => alert(e.message));
-  }
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
 
-  componentDidMount() {
-    this.pagination();
-  }
-
-  abortFetching() {
-    console.log("revoking fetch function");
-    controller.abort();
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
   }
 
   renderBody() {
-    let { searchTerm, result, pagination } = this.state;
-    // console.losg("render ===> ", searchTerm);
-    let pages = [];
-    for (let i = 0; i < pagination; i++) pages.push(i);
-    const renderPagination = pages.map(v => {
-      return (
-        <li
-          key={v}
-          onClick={() => {
-            this.pagination(v);
-          }}
-          style={{
-            display: "inline",
-            margin: "auto 1px",
-            cursor: "pointer",
-            padding: "1px",
-            border: "thin solid grey"
-          }}
-        >
-          {v + 1}
-        </li>
-      );
-    });
+    const { coords } = this.state;
     return (
       <div>
-        <center>
-          <ul>{renderPagination}</ul>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={e => {
-              // this.search(e.target.value);
-              this.setState({ searchTerm: e.target.value, result: [] });
-            }}
-          />
-          <button
-            onClick={() => {
-              this.search(searchTerm);
-            }}
-          >
-            Search
-          </button>
-          <button onClick={this.abortFetching}>Cancel fetching</button>
-        </center>
-        <ol>
-          {result
-            ? result.map(v => {
-                return (
-                  <li key={v.objectID}>
-                    <p>{v.title}</p>
-                  </li>
-                );
-              })
-            : null}
-          {/* {result && (
-            <button
-              onClick={() => {
-                this.setState({ i: ++this.state.i });
-                this.search();
-              }}
-            >
-              Load More
-            </button>
-          )} */}
-        </ol>
+        {console.log(coords.latitude, coords.longitude)}
+        <button onClick={this.signWithFacebook}>Signin with Facebook</button>
+        <MyMapComponent
+          updateCoords={this.updateCoords}
+          coords={coords}
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
@@ -188,15 +106,37 @@ class App extends Component {
   }
 
   render() {
-    // const { text } = this.state;
     return (
-      <div>
-        <div className="App">{this.renderHeader()}</div>
+      <div className="App">
+        {this.renderHeader()}
         {this.renderBody()}
-        {/* <div className="App">{this.renderFooter()}</div> */}
+        {/* {this.renderFooter()} */}
       </div>
     );
   }
 }
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={15}
+      center={{ lat: props.coords.latitude, lng: props.coords.longitude }}
+    >
+      {props.isMarkerShown &&
+        props.coords && (
+          <Marker
+            draggable={true}
+            onDragEnd={res => {
+              props.updateCoords(res.latLng.lat(), res.latLng.lng());
+            }}
+            position={{
+              lat: props.coords.latitude,
+              lng: props.coords.longitude
+            }}
+          />
+        )}
+    </GoogleMap>
+  ))
+);
 
 export default App;
